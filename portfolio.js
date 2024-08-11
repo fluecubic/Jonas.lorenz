7849ec02ce3cc136c950792ca93b76c883a25bdf
fluecubic <jonas.lorenz1234567890@gmail.com>
2024-08-11 16:27:30 +0200

    oma

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,9 @@
 let blmchnpfrdchnimg = document.getElementById("blmchnpfrdchnimg");
 let psswrtchckrimg = document.getElementById("psswrtchckrimg");
 
-blmchnpfrdchnimg.onclick = window.open("https://fluecubic.github.io/Bluemchen-Pferdchen_Club/");
-psswrtchckrimg.onclick = window.open("https://fluecubic.github.io/Passwortchecker/");
+function open(url) {
+    window.open(url)
+}
+
+blmchnpfrdchnimg.onclick = open("https://fluecubic.github.io/Bluemchen-Pferdchen_Club/");
+psswrtchckrimg.onclick = open("https://fluecubic.github.io/Passwortchecker/");
